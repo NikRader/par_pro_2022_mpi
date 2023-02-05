bf614fe431697b45462849817b135aa13d063a0c
NKudryavtsev <nikitakudryavtsev31@mail.ru>
2023-02-05 10:19:15 +0300

    work

---
+++ modules/task_2/kudryavtsev_n_sleeping_barber/main.cpp
@@ -0,0 +1,61 @@
+// Copyright 2022 Kudryavtsev Nikita
+#include <mpi.h>
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include "../../../modules/task_2/kudryavtsev_n_sleeping_barber/sleeping_barber.h"
+
+
+TEST(Barber, TEST_ASSSERT1) {
+    int visite_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &visite_size);
+    if (visite_size < 2) {
+        ASSERT_ANY_THROW(load());
+    }
+}
+
+TEST(Barber, TEST_ASSSERT2) {
+    int visite_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &visite_size);
+    if (visite_size > 1) {
+        ASSERT_NO_THROW(load());
+    }
+}
+
+TEST(Barber, TEST_ASSSERT3) {
+    int visite_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &visite_size);
+    if (visite_size > 5) {
+        ASSERT_NO_THROW(load());
+    }
+}
+
+TEST(Barber, TEST_ASSSERT4) {
+    int visite_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &visite_size);
+    if (visite_size > 10) {
+        ASSERT_NO_THROW(load());
+    }
+}
+
+TEST(Barber, TEST_ASSSERT5) {
+    int visite_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &visite_size);
+    if (visite_size > 50) {
+        ASSERT_NO_THROW(load());
+    }
+}
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners = ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
+
